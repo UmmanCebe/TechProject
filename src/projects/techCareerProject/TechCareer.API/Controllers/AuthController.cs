69077b6d97ca3cda25e0bef9657d4264653789fc
OmerFarukD <89133026+OmerFarukD@users.noreply.github.com>
2024-11-25 03:19:41 +0300

    Add project files.

---
+++ src/projects/techCareerProject/TechCareer.API/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Core.Security.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using TechCareer.Service.Abstracts;
+
+namespace TechCareer.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(IAuthService _authService) : ControllerBase
+{
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody]UserForLoginDto dto,CancellationToken cancellationToken)
+    {
+        var result = await _authService.LoginAsync(dto, cancellationToken);
+        return Ok(result);
+    }
+}
---
+++ src/projects/techCareerProject/TechCareer.Service/Abstracts/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Core.Security.Dtos;
+using Core.Security.JWT;
+
+namespace TechCareer.Service.Abstracts;
+
+public interface IAuthService
+{
+    Task<AccessToken> LoginAsync(UserForLoginDto dto,CancellationToken cancellationToken);
+    Task<AccessToken> RegisterAsync(UserForRegisterDto dto,CancellationToken cancellationToken);
+}
