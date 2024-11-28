2384d6cb07866d1106439109a4a69fd313f6a5c7
Valtunun <emirhantali123@gmail.com>
2024-11-28 13:29:28 +0300

    VideoDto created

---
+++ src/projects/techCareerProject/TechCareer.Models/Dtos/VideoEducation/RequestDto/VideoEducationCreateRequest.cs
@@ -0,0 +1,19 @@
+﻿using TechCareer.Models.Enums;
+
+namespace TechCareer.Models.Dtos.VideoEducation.RequestDto;
+
+public class VideoEducationCreateRequest
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public double TotalHour { get; set; }
+    public bool IsCertified { get; set; }
+    public Level Level { get; set; }
+    public string ImageUrl { get; set; }
+    public string ProgrammingLanguage { get; set; }
+}
+
+
+
+
+
