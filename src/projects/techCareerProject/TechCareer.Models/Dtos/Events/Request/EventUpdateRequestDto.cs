bc68dc9fd7971d32e7288f0e386ebd2e795063fa
fratyldrm <104390157+fratyldrm@users.noreply.github.com>
2024-11-28 13:57:33 +0300

    EventCreateReguest ,EventUpdateReguest ,EventResponse Dto Added

---
+++ src/projects/techCareerProject/TechCareer.Models/Dtos/Events/Request/EventUpdateRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechCareer.Models.Dtos.Events.Request;
+
+public sealed  record EventUpdateRequestDto
+{
+    public string Title { get; init; }
+    public string Description { get; init; }
+    public string ImageUrl { get; init; }
+    public DateTime StartDate { get; init; }
+    public DateTime EndDate { get; init; }
+    public DateTime ApplicationDeadline { get; init; }
+    public string ParticipationText { get; init; }
+    public int CategoryId { get; init; }
+
+}
