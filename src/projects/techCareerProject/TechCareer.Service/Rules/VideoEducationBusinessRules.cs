a947a8a9d58f4b78b0e6ac47a3d7a3b78b062a4b
Aygün Bayır <turkoaygun@gmail.com>
2024-11-29 10:56:22 +0300

    video education servisi eklendi

---
+++ src/projects/techCareerProject/TechCareer.Service/Rules/VideoEducationBusinessRules.cs
@@ -0,0 +1,9 @@
+﻿using Core.CrossCuttingConcerns.Rules;
+using TechCareer.DataAccess.Repositories.Abstracts;
+
+namespace TechCareer.Service.Rules;
+
+public sealed class VideoEducationBusinessRules(IVideoEducationRepository _videoEducationRepository) : BaseBusinessRules
+{
+
+}
