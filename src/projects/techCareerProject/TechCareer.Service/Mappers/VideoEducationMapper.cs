36ce21fb92a2675d1e3a5e0c3a84dc6f5442dcc7
Aygün Bayır <turkoaygun@gmail.com>
2024-11-29 15:27:03 +0300

    VideoEducation varlığının Dto kullanımı ve Dto validasyonları implement edildi

---
+++ src/projects/techCareerProject/TechCareer.Service/Mappers/VideoEducationMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TechCareer.Models.Dtos.VideoEducation.RequestDto;
+using TechCareer.Models.Dtos.VideoEducation.ResponseDto;
+using TechCareer.Models.Entities;
+
+namespace TechCareer.Service.Mappers;
+
+public class VideoEducationMapper : Profile
+{
+    public VideoEducationMapper()
+    {
+        CreateMap<VideoEducation, VideoEducationResponse>();
+        CreateMap<VideoEducationCreateRequest, VideoEducation>();
+        CreateMap<VideoEducationUpdateRequest, VideoEducation>();
+    }
+}
