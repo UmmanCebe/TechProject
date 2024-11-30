65faa04728e72425291b04913f434d38360f4482
Valtunun <emirhantali123@gmail.com>
2024-11-30 21:02:09 +0300

    Update category validation added

---
+++ src/projects/techCareerProject/TechCareer.Service/Validations/Category/UpdateCategoryValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechCareer.Models.Dtos.Categories.RequestDto;
+
+namespace TechCareer.Service.Validations.Category;
+
+public class UpdateCategoryValidator : AbstractValidator<UpdateCategoryRequestDto>
+{
+
+    public UpdateCategoryValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Kategori adı boş olamaz.")
+            .Length(3, 50).WithMessage("Kategori adı 3 ile 50 karakter arasında olmalıdır.");
+
+
+
+
+
+
+
+    }
+}
